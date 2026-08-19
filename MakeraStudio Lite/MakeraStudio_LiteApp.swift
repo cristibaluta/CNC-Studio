ed5a1161191a6ad956a1f08f164656a0ff9ed6f5
Cristian Baluta <cristi.baluta@gmail.com>
2026-08-19 07:02:48 +0300

    Initial Commit

---
+++ MakeraStudio Lite/MakeraStudio_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakeraStudio_LiteApp.swift
+//  MakeraStudio Lite
+//
+//  Created by Cristian Baluta on 19.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct MakeraStudio_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
